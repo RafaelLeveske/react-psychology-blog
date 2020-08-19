9702598e6c67e430a55e74ed9571e004d1d185af
Rafael Vieira <61164438+RafaelLeveske@users.noreply.github.com>
2020-08-19 10:49:08 -0300

    Initial commit

---
+++ src/config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'docker',
+  password: 'docker',
+  database: 'node-sequelize',
+  define: {
+    timestamps: true,
+  },
+};
